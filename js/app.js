3626413177fb7a0cbd34c6169c4d6b14271138e6
MohannadAteyyat <ateyyatmohanad@gmail.com>
2021-06-01 11:25:53 +0300

    finish

---
+++ js/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+document.querySelector('Render')
+let place = []
+function Places (placeName , tripPlace , typeOftransport){
+
+this.placeName = placeName
+this.tripPlace = tripPlace 
+this.typeOftransport = typeOftransport 
+place.push(this.Places)
+}
+
+console.log(place);
+new Places('HAWAII', 'US' , 'Cruise')
+new Places('ITALY' , 'Europ' , 'Train')
+new Places('LONDON' , 'Uk' , 'Flight')
+new Places('MALAYSIA' , 'Asia' , 'Flight')
+new Places('PARIS', 'France' , 'Train')
+new Places('SLOVAKIA', 'Europ' , 'Car')
+
+
+
+
+function render () {
+
+    
+}
